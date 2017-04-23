528f4f3297288b55980e9effd8cdd32a09720a2d
julia <j@knopka.com>
2017-04-24 00:53:49 +0500

    Initial commit

---
+++ Hr/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hr
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
