825b308d1cf169247257571ebe86f9e4334fa3c8
julia <j@knopka.com>
2017-04-26 13:12:23 +0500

    quicksort1 partiotion

---
+++ TestHr/Quicksort1PartitionTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Hr;
+using NUnit.Framework;
+
+namespace TestHr
+{
+    public class Quicksort1PartitionTest
+    {
+        [Test]
+        public void Simple()
+        {
+            Assert.That(Quicksort1Partition.Partition(new int[] {4, 5, 3, 7, 2}), Is.EquivalentTo(new int[] {3, 2, 4, 5, 7}));
+        }
+
+        [Test]
+        public void Null()
+        {
+            Assert.That(Quicksort1Partition.Partition(null), Is.EqualTo(new int[]{}));
+        }
+    }
+}
